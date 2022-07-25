da0601fda83f729aca4d400ae46c7c355c8e5d45
YenNgoc <yenngocliu@gmail.com>
2022-07-25 23:46:45 +0700

    ck

--- conn.php
+++ conn.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "data";
+ $severName  = "bxba8rfhdrrlbtci7ykk-mysql.services.clever-cloud.com";
+ $userName = "uxqdx1hocxdub2ek";
+ $passWord = "ILfJpUgdLBFr26tWwkoq";
+ $databaseName = "bxba8rfhdrrlbtci7ykk";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($severName , $userName, $passWord, $databaseName);
 
 
 
